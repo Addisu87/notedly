220977326b609c822f5e220a9d8e09d7a7fd6f7c
Addisu <addisuhaile87@gmail.com>
2023-05-11 16:50:52 +0300

    Update graphQL queries path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+// Run the server on a port specified in our .env file or port 4000
+const port = process.env.PORT || 4000;
+
+// Construct a schema, using GraphQL's schema language
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+server.start().then((res) => {
+  server.applyMiddleware({ app, path: "/api" });
+  app.listen({ port }, () =>
+    console.log(
+      `GraphQL Server running on http://localhost:${port}${server.graphqlPath}`
+    )
+  );
 });
-
-app.listen(4000, () => console.log("Listening on port 4000!"));
