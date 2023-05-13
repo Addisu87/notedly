057c5b0cf389f1fbb3bba9d136d3838c9fc754fe
Addisu <addisuhaile87@gmail.com>
2023-05-13 18:57:17 +0300

    Update the newNote mutations

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
 require("dotenv").config();
+
 const db = require("./db");
 const models = require("./models");
 
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
-
 // Store the DB_HOST value as a variable
 const DB_HOST = process.env.DB_HOST;
-
-let notes = [
-  {
-    id: "1",
-    content: "This is a note",
-    author: "Adam Scott",
-  },
-  {
-    id: "2",
-    content: "This is another note!",
-    author: "Harlow Everly",
-  },
-  {
-    id: "3",
-    content: "This is a third note!",
-    author: "Riley Harrison",
-  },
-];
 
 // Construct a schema, using GraphQL's schema language
 const typeDefs = gql`
@@ -51,25 +33,20 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find((note) => note.id === args.id);
-    },
-
     notes: async () => {
       return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
     },
   },
 
   Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
         author: "Addisu Haile",
-      };
-      notes.push(noteValue);
-      return noteValue;
+      });
     },
   },
 };
