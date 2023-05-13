1aa81dfe6001cd7e7fa69d2c25c1e12f74239516
Addisu <addisuhaile87@gmail.com>
2023-05-13 16:39:38 +0300

    Create a Note model

---
+++ src/models/note.js
@@ -0,0 +1,11 @@
+// Require the mongoose library
+const mongoose = require("mongoose");
+
+// Define the note's database schema
+const noteSchema = new mongoose.Schema();
+
+// Define the "Note" model with the schema
+const Note = mongoose.model("Note", noteSchema);
+
+// Export the model
+module.exports = Note;
