7dd3c1910e9cdc7e24fe5fed51b71b71093c7ac7
Addisu <addisuhaile87@gmail.com>
2023-05-21 17:57:07 +0300

    Add signUp and signIn mutations

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,13 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {
+  AuthenticationError,
+  ForbiddenError,
+} = require("apollo-server-express");
+require("dotenv").config();
+
+const gravatar = require("gravatar");
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -29,5 +39,50 @@ module.exports = {
         new: true,
       }
     );
+  },
+
+  signUp: async (parent, { username, email, password }, { models }) => {
+    // normalize email address
+    email = email.trim().toLowerCase();
+    // hash the password
+    const hashed = await bcrypt.hash(password, 10);
+    // create the gravatar url
+    const avatar = gravatar.url(email);
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed,
+      });
+
+      // Create and return the json web token
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.error(err);
+      // If there's a problem creating the account, throw an error
+      throw new Error("There was a problem creating your account.");
+    }
+  },
+
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      // normalize email address
+      email = email.trim().toLowerCase();
+    }
+
+    const user = await models.User.findOne({ $or: [{ email }, { username }] });
+    // if no user is found, throw an authentication error
+    if (!user) {
+      throw new AuthenticationError("Incorrect username or password.");
+    }
+    // if the passwords don't match, throw an authentication error
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError("Incorrect username or password.");
+    }
+
+    // create and return the json web token
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   },
 };
