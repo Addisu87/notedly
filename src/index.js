5e35099287d4bbd74f678079f865c32bc743c7d5
Addisu <addisuhaile87@gmail.com>
2023-05-22 19:53:49 +0300

    Add user authentication

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const db = require("./db");
 const models = require("./models");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+const jwt = require("jsonwebtoken");
 
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
@@ -17,13 +18,32 @@ const app = express();
 // Connect to the database
 db.connect(DB_HOST);
 
+// get the user info from a JWT
+const getUser = (token) => {
+  if (token) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      // if the token is invalid, throw an error
+      throw new Error("Session is invalid");
+    }
+  }
+};
+
 // Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    // Add the db models to the context
-    return { models };
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // for now, let's log the user to the console
+    console.log(user);
+    // Add the db models and the user to the context
+    return { models, user };
   },
 });
 
