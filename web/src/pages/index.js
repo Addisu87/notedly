7037ac919c36c662a8820924a89de3ec6c61ade4
Addisu <addisuhaile87@gmail.com>
2023-06-05 10:05:18 +0300

    Create a layout for wrap components

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./home";
 import MyNotes from "./mynotes";
 import Favorites from "./favorites";
+import Layout from "../components/Layout";
 
 // define routes
 const Pages = () => {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/mynotes" element={<MyNotes />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
+      {/* Wrap our routes within a layout component*/}
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/mynotes" element={<MyNotes />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
