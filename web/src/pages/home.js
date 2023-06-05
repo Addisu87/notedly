6dbd9dfd01d4b6e0e794c2eec69ca04214195ef9
Addisu <addisuhaile87@gmail.com>
2023-06-05 07:08:00 +0300

    Create Routing

---
+++ web/src/pages/home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Notedly</h1>
+      <p>This is the home page</p>
+      {/* add a list of links */}
+      <ul>
+        <li>
+          <Link to="/mynotes" />
+          My Notes
+        </li>
+        <li>
+          <Link to="/favorites" />
+          Favorites
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
